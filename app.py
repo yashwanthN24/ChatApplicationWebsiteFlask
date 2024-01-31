1275bb6fe694bf96554e5063ea2324f4158e13d0
yashwanth <yashwanthnaddana2002@gmail.com>
2024-01-31 13:04:29 +0530

    modified requirements and gitignore

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY') 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI' ,'sqlite:///chat.db' )
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL' )
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
